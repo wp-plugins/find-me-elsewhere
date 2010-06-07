a207a657a5f0306700cb481f51d43e37439de13a
wpease <wpease@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-07 08:45:10 +0000

    git-svn-id: https://plugins.svn.wordpress.org/find-me-elsewhere/trunk@249670 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- script/fmew_stylethree.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function(){
-	jQuery("#fmew").toggle(function(){
-		jQuery("#fmew").find("ul").slideDown("slow");
-	},
-	function(){
-		jQuery("#fmew").find("ul").slideUp("slow");
-	});
-})
